e68a50ebd773c5efd97d26c9fdcd69de6f51d277
Mehmet (P.R.M.) Misset <mehmet.misset@gmail.com>
2025-11-29 10:39:09 +0100

    refactor: rename id_transformation_dataset_attribute to id_transformation_part_attribute for consistency

--- meta-data-model/1-Meta-Data-Model/4-DTA-Data-Transformation-Area/tsa/tsa_transformation_part_attribute.sql
+++ meta-data-model/1-Meta-Data-Model/4-DTA-Data-Transformation-Area/tsa/tsa_transformation_part_attribute.sql
@@ -3,7 +3,7 @@
     /* Data Attributes */
     id_model                            CHAR(32)      NULL,
     id_transformation_part              CHAR(32)      NULL,
-    id_transformation_dataset_attribute CHAR(32)      NULL,
+    id_transformation_part_attribute    CHAR(32)      NULL,
     cd_transformation_part_clause_type  NVARCHAR(32)  NULL,
     cd_source_alias                     NVARCHAR(32)  NULL,
     id_source_model                     CHAR(32)      NULL,
