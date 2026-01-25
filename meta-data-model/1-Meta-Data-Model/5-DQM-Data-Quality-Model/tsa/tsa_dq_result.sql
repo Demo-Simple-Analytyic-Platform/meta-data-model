8bd1067a88fe695dabfce811c61524c875c34976
Mehmet (P.R.M.) Misset <mehmet.misset@gmail.com>
2026-01-25 23:54:17 +0100

    feat: Add metadata columns to tsa_dq_result and tsa_dq_totals tables so these can follow the transformation pattern

--- meta-data-model/1-Meta-Data-Model/5-DQM-Data-Quality-Model/tsa/tsa_dq_result.sql
+++ meta-data-model/1-Meta-Data-Model/5-DQM-Data-Quality-Model/tsa/tsa_dq_result.sql
@@ -10,5 +10,15 @@
     id_dataset_5_bk      CHAR(32) NULL,
     dt_dq_result         DATETIME NULL,
     id_dq_result_status  CHAR(32) NULL,
+    
+    /* Metadata dq_requirements */
+    meta_dt_valid_from DATETIME NOT NULL,
+    meta_dt_valid_till DATETIME NOT NULL,
+    meta_is_active     BIT      NOT NULL,
+    meta_ch_rh         CHAR(32) NOT NULL,
+    meta_ch_bk         CHAR(32) NOT NULL,
+    meta_ch_pk         CHAR(32) NOT NULL, 
+    meta_dt_created    DATETIME NOT NULL DEFAULT GETDATE(), 
+
 );
 GO
--- meta-data-model/1-Meta-Data-Model/5-DQM-Data-Quality-Model/tsa/tsa_dq_total.sql
+++ meta-data-model/1-Meta-Data-Model/5-DQM-Data-Quality-Model/tsa/tsa_dq_total.sql
@@ -13,5 +13,14 @@
     ni_oos                    INT       NULL,
     pr_oos                    DEC(24,6) NULL,
 
+    /* Metadata dq_requirements */
+    meta_dt_valid_from DATETIME NOT NULL,
+    meta_dt_valid_till DATETIME NOT NULL,
+    meta_is_active     BIT      NOT NULL,
+    meta_ch_rh         CHAR(32) NOT NULL,
+    meta_ch_bk         CHAR(32) NOT NULL,
+    meta_ch_pk         CHAR(32) NOT NULL, 
+    meta_dt_created    DATETIME NOT NULL DEFAULT GETDATE(), 
+
 );
 GO
